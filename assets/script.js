972ece5178e3328518e4e1bc4d39d06ec025108e
jwbeach <jacobwbeach@gmail.com>
2022-08-04 16:57:43 -0600

    checkboxes made with parameters for password set

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,32 @@
 var passSlider = document.getElementById("passLength");
 var slideOutput = document.getElementById("slideValue");
 slideOutput.innerHTML = passSlider.value;
+var password=document.getElementById("password");
+        // ^does this need a space on both sides?
+
 
 passSlider.oninput = function() {
+    //function to read out slider value in real time
   slideOutput.innerHTML = this.value;
 }
 
+function readPara () {
+    //function for reading special parameters  
+}
 
+
+function genPass() {
+    // var for each group
+    // numbers
+    // text
+    // text caps
+    // special characters
+}
 
 
 // Password Pathway
 // Grab Parameters
+
+//hit generate
+
+//create modale to display and copy
