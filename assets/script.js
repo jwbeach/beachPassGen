7a192f4526007ecdd8bb3b12a4e57000028f0656
jwbeach <jacobwbeach@gmail.com>
2022-08-08 22:02:05 -0600

    parameters create single string to pull pass gen from

--- assets/script.js
+++ assets/script.js
@@ -2,66 +2,52 @@ var passSlider = document.getElementById("passLength");
 var slideOutput = document.getElementById("slideValue");
 slideOutput.innerHTML = passSlider.value;
 var password=document.getElementById("password");
-        // ^does this need a space on both sides?
-
-//query selector for button
-var generateButton = document.querySelector('#button1');
-
-var para1DOM = document.getElementById("para1");
-var para2DOM = document.getElementById("para2")
-var para3DOM = document.getElementById("para3"); 
-var para4DOM = document.getElementById("para4");
-
-
-
+var generateButton = document.querySelector('#button1');//query selector for button
+var para1DOM = document.getElementById("para1");//"1234567890"
+var para2DOM = document.getElementById("para2");//"abcdefghijklmnopqrstuvwxyz"
+var para3DOM = document.getElementById("para3");//"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var para4DOM = document.getElementById("para4");//"!@#$%^&*()_+{}|:?"
 
 passSlider.oninput = function() {
-    //function to read out slider value in real time
+// reads out slider value in real time
   slideOutput.innerHTML = this.value;
 }
 
-//function for reading special parameters   then F:genPass
 function checkPara () {
-    const passArray = [""]
-    var numbers = "1234567890";
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var specialChar = "!@#$%^&*()_+{}|:?";
+var numbers = "1234567890";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!@#$%^&*()_+{}|:?";
+const passArray = [""] //stored intial pass from parameter checkboxes 
 
-        if(document.getElementById("para1").checked) {
+    //parameters for password, adds to passArray
+    if(document.getElementById("para1").checked) {
             passArray.push(numbers); 
         }
-        if(document.getElementById("para2").checked) {
+    if(document.getElementById("para2").checked) {
             passArray.push(lowerCase); 
         }
-        if(document.getElementById("para3").checked) {
+    if(document.getElementById("para3").checked) {
             passArray.push(upperCase); 
         }
-        if(document.getElementById("para4").checked) {
+    if(document.getElementById("para4").checked) {
             passArray.push(specialChar); 
         }
-        console.log(passArray)
-
-    }
 
 
-function genPass() {
-    // var numbers = "1234567890";
-    // var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    // var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // var specialChar = "!@#$%^&*()_+{}|:?";
-    
+const passString = passArray.join("");//puts all selected parameters into a single string without commas 
+console.log(passString);
 
-
-    //var item = items[Math.floor(Math.random()*items.length)];
-    //^pull from the array with length
 }
 
-generateButton.addEventListener("click", checkPara);
-
-// Password Pathway
-// Grab Parameters
-
-//hit generate
+function genPass() {
+//var item = items[Math.floor(Math.random()*items.length)];
+//pull from the passString X amount of times
+}
 
 //create modale to display and copy
+
+generateButton.addEventListener("click", checkPara);//generates pass on click
+
+
+
