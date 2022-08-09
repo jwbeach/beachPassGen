f3bc70bdc3f615aa4607f7e25cb01c05ef442626
jwbeach <jacobwbeach@gmail.com>
2022-08-08 18:24:39 -0600

    java for para and pushing to array

--- assets/script.js
+++ assets/script.js
@@ -4,25 +4,60 @@ slideOutput.innerHTML = passSlider.value;
 var password=document.getElementById("password");
         // ^does this need a space on both sides?
 
+//query selector for button
+var generateButton = document.querySelector('#button1');
+
+var para1DOM = document.getElementById("para1");
+var para2DOM = document.getElementById("para2")
+var para3DOM = document.getElementById("para3"); 
+var para4DOM = document.getElementById("para4");
+
+
+
 
 passSlider.oninput = function() {
     //function to read out slider value in real time
   slideOutput.innerHTML = this.value;
 }
 
-function readPara () {
-    //function for reading special parameters  
-}
+//function for reading special parameters   then F:genPass
+function checkPara () {
+    const passArray = [""]
+    var numbers = "1234567890";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var specialChar = "!@#$%^&*()_+{}|:?";
+
+        if(document.getElementById("para1").checked) {
+            passArray.push(numbers); 
+        }
+        if(document.getElementById("para2").checked) {
+            passArray.push(lowerCase); 
+        }
+        if(document.getElementById("para3").checked) {
+            passArray.push(upperCase); 
+        }
+        if(document.getElementById("para4").checked) {
+            passArray.push(specialChar); 
+        }
+        console.log(passArray)
+
+    }
 
 
 function genPass() {
-    // var for each group
-    // numbers
-    // text
-    // text caps
-    // special characters
+    // var numbers = "1234567890";
+    // var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    // var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // var specialChar = "!@#$%^&*()_+{}|:?";
+    
+
+
+    //var item = items[Math.floor(Math.random()*items.length)];
+    //^pull from the array with length
 }
 
+generateButton.addEventListener("click", checkPara);
 
 // Password Pathway
 // Grab Parameters
